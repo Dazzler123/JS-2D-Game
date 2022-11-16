ee1e32a68a1baf7ce4b421e8936323a18bd827df
Dazzler123 <dasinduhewagamage@gmail.com>
2022-11-16 21:13:46 +0530

    Character running animation implemented

--- controller/Character_Behaviour.js
+++ controller/Character_Behaviour.js
@@ -1,17 +1,19 @@
 var ninja = document.getElementById("character");
 var idleImageNum = 0;
 var idleAnimationIndex = 0
+var runImageNum = 0;
+var runAnimationIndex = 0;
 
 //start idle animation on load
 $('#game_container').onload = idleAnimationStart();
 
 function idleAnimation() {
     // after all idle images are loaded
-    if(idleImageNum == 10) {
+    if (idleImageNum == 10) {
         idleImageNum = 0;
     }
     // set image
-    ninja.src = "assets/images/characters/Idle"+idleImageNum+".png";
+    ninja.src = "assets/images/characters/Idle" + idleImageNum + ".png";
 
     // to next idle image
     idleImageNum++;
@@ -19,4 +21,37 @@ function idleAnimation() {
 
 function idleAnimationStart() {
     idleAnimationIndex = setInterval(idleAnimation, 150);
+}
+
+
+// character running animation
+function runAnimation() {
+    // after all running images are loaded
+    if (runImageNum == 10) {
+        runImageNum = 0;
+    }
+    //set image
+    ninja.src = "assets/images/characters/Run" + runImageNum + ".png";
+
+    // to next running image
+    runImageNum++;
+}
+
+function runAnimationStart() {
+    runAnimationIndex = setInterval(runAnimation, 100);
+    //stop idle animation
+    clearInterval(idleAnimationIndex);
+}
+
+function keyCheck(event) {
+    // enter = 13;
+    var keyCode = event.which;
+
+    //if enter key is pressed
+    if (keyCode == 13) {
+        if (runImageNum == 0) {
+            runAnimationStart();
+        }
+    }
+
 }
