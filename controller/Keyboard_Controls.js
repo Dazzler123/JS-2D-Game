e3d42b89c98f5430263843f14a84018ef43ede83
Dazzler123 <dasinduhewagamage@gmail.com>
2022-11-23 13:06:45 +0530

    Blinker effect added

--- controller/Keyboard_Controls.js
+++ controller/Keyboard_Controls.js
@@ -1,9 +1,15 @@
+var enterCtrlBlink = 0;
+
 //when keys are pressed
 function keyCheck(event) {
     var keyCode = event.which;
 
     //if enter key is pressed
     if (keyCode == 13) {
+        //hide enter ctrl title
+        $('#enter_ctrl').remove();
+
+        //start ninja running animation
         if (runImageNum == 0) {
             runAnimationStart();
         }
@@ -34,3 +40,10 @@ function keyCheck(event) {
         wolfAnimationIndex = setInterval(wolfAnimation, 100);
     }
 }
+
+//control instructions blink effect
+function blink_text() {
+    $('#enter_ctrl').fadeOut(100);
+    $('#enter_ctrl').fadeIn(100);
+}
+enterCtrlBlink = setInterval(blink_text, 500);
